5f2d48fe3412eac25044a5976a58089a110bafda
Jackson <jpwoods92@gmail.com>
2018-09-24 15:11:01 -0400

    clean up code

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,24 +1,11 @@
 import * as MessageAPIUtil from '../util/message_api_util'
 export const RECEIVE_ALL_MESSAGES = 'RECEIVE_ALL_MESSAGES'
-export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE'
 
 const receiveAllMessages = (messages) => {
   return {
     type: RECEIVE_ALL_MESSAGES,
     messages
   }
-}
-
-export const receiveMessage = (message) => {
-  return {
-    type: RECEIVE_MESSAGE,
-    message
-  }
-}
-
-export const createMessage = (message) => dispatch => {
-  return (
-    MessageAPIUtil.createMessage(message))
 }
 
 export const fetchMessages = (roomId) => dispatch => {
