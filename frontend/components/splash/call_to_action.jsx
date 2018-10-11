35808bff76399341bf75cb6aa42c0f5781e8f8af
Jackson <jpwoods92@gmail.com>
2018-10-11 16:50:56 -0400

    center content

--- frontend/components/splash/call_to_action.jsx
+++ frontend/components/splash/call_to_action.jsx
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 export default () => {
   return (
     <div className='call-to-action-card'>
-      <img id='work-img' alt='slackr-work-img' src={window.workImg} />
-      <div className='call-to-action-body'>
-        <h2 id='call-to-action-title' >Where Work Happens</h2>
-        <p id='call-to-action-body-text'>When your team needs to slack off during a project,
+      <div className='content'>
+        <img id='work-img' alt='slackr-work-img' src={window.workImg} />
+        <div className='call-to-action-body'>
+          <h2 id='call-to-action-title' >Where Work Happens</h2>
+          <p id='call-to-action-body-text'>When your team needs to slack off during a project,
           fire a new employee, deploy some bad code, finalize next year's debt,
           plan your next office downsizing, and more, Slackr has you covered.</p>
-        <Link id='get-started-button' to='/signup'>GET STARTED</Link>
-        <p id='sign-in-text'>Already using Slack? <Link id='sign-in-link' to='/login'>Sign in.</Link></p>
+          <Link id='get-started-button' to='/signup'>GET STARTED</Link>
+          <p id='sign-in-text'>Already using Slack? <Link id='sign-in-link' to='/login'>Sign in.</Link></p>
+        </div>
       </div>
     </div>
   )
